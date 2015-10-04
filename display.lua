a9748929973b37768e16952327c2c232a65acab3
= <=>
2015-10-04 17:50:53 +0100

    attempt at centering

--- display.lua
+++ display.lua
@@ -2,8 +2,16 @@ local screen = peripheral.find("monitor")
 
 assert(screen ~= nil)
 
-local screenX, screenY = screen.getSize()
+local screen_x, screen_y = screen.getSize()
 
-local message = {...}
+local args = {...}
+local message = args[1]
+assert(message ~= nil)
+local size = screen_y
 
+local padding = (screen_x - message:len()*size)/2
+if padding < 1 then padding = 1 end
+print("printing at scale "..size.."with padding"..padding+1)
+screen.setTextScale(size)
+screen.setCursorPos(padding+1, 1)
 screen.write(message)
